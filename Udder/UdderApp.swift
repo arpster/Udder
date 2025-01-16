e3969520f6c580faeb2fd42f5e5b1be71d25d72c
Stacey Arp <develop@mediaspringboard.com>
2025-01-16 08:33:34 -0500

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Stacey Arp on 1/16/25.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
